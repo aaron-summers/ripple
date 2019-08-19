224eb5c78392573f3ab865ca266dcb12a0c225a1
Aaron Summers <aaron.summers2797@gmail.com>
2019-08-19 10:15:07 +0100

    initial commit

---
+++ ripple_api/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :show]
+      post '/login', to: 'auth#create'
+      get '/validate', to: 'auth#validate_token'
+    end
+  end
+end
