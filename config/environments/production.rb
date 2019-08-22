dddb062a2b8883d153dce646e0fda22df9d68c6d
Aaron Summers <aaron.summers2797@gmail.com>
2019-08-22 10:48:21 +0100

    setting up redis for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.middleware.user ChatActionCable
+  config.web_socket_server_url = "wss://ripple-server.herokuapp.com/"
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
